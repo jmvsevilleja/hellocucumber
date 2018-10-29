f4a494dc5c84e41bde0db7c79a13e8789bb7df62
jsevilleja <jmvsevilleja@gmail.com>
2018-10-29 14:39:18 +0800

    using scenario outline

--- src/test/java/hellocucumber/Stepdefs.java
+++ src/test/java/hellocucumber/Stepdefs.java
@@ -9,6 +9,9 @@ import static org.junit.Assert.*;
 
 class IsItFriday {
     static String isItFriday(String today) {
+        if (today.equals("Friday")) {
+            return "TGIF";
+        }
         return "Nope";
     }
 }
@@ -17,11 +20,10 @@ public class Stepdefs {
     private String today;
     private String actualAnswer;
 
-    @Given("^today is Sunday$")
-    public void today_is_Sunday() {
-        this.today = "Sunday";
+    @Given("^today is \"([^\"]*)\"$")
+    public void today_is(String today) {
+        this.today = today;
     }
-
 
     @When("^I ask whether it's Friday yet$")
     public void i_ask_whether_is_s_Friday_yet() {
